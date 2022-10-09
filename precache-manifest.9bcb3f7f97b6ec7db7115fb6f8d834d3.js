7d4fc904c2e911bfdef4dec8b4697714aa3e7b93
Josip Leko <48994756+kkonne@users.noreply.github.com>
2022-10-09 17:53:38 +0200

    Deploy attempt

---
+++ precache-manifest.9bcb3f7f97b6ec7db7115fb6f8d834d3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185058caa98c298b26f3",
+    "url": "/juko-landing-deploy/css/app.e821aa82.css"
+  },
+  {
+    "revision": "9922fcae941e7132c21f",
+    "url": "/juko-landing-deploy/css/chunk-0471cfae.0660761d.css"
+  },
+  {
+    "revision": "03979835ea04b8b39629",
+    "url": "/juko-landing-deploy/css/chunk-2d6697bc.80fb2008.css"
+  },
+  {
+    "revision": "1c9031c617ac1fb4ee7a",
+    "url": "/juko-landing-deploy/css/chunk-97d2034c.9dac6b4e.css"
+  },
+  {
+    "revision": "78d685e66440f4bebbe6",
+    "url": "/juko-landing-deploy/css/chunk-c5c8e94a.abedac41.css"
+  },
+  {
+    "revision": "75ac392696b8aeee2c07a961c1861110",
+    "url": "/juko-landing-deploy/img/company-logo.75ac3926.svg"
+  },
+  {
+    "revision": "7b4a2e52d8da871ab2af32e8d8446c50",
+    "url": "/juko-landing-deploy/img/error-icon.7b4a2e52.svg"
+  },
+  {
+    "revision": "218d6e46b3a6b1bff857d094d947a945",
+    "url": "/juko-landing-deploy/img/light-icon.218d6e46.png"
+  },
+  {
+    "revision": "15648d18576a477bdd7a7f28c9d9accf",
+    "url": "/juko-landing-deploy/img/location-icon.15648d18.svg"
+  },
+  {
+    "revision": "26fbb2fbd97fa3e7ef7fb0a31176a025",
+    "url": "/juko-landing-deploy/img/moon-icon.26fbb2fb.svg"
+  },
+  {
+    "revision": "329313cdc8765928e44d71f89eece0ac",
+    "url": "/juko-landing-deploy/img/phone-icon.329313cd.svg"
+  },
+  {
+    "revision": "ff5a74432b1057d361fda519b0d404bc",
+    "url": "/juko-landing-deploy/img/success-icon.ff5a7443.svg"
+  },
+  {
+    "revision": "69813ee5e9ea0bf57676409d6613b796",
+    "url": "/juko-landing-deploy/index.html"
+  },
+  {
+    "revision": "185058caa98c298b26f3",
+    "url": "/juko-landing-deploy/js/app.9bd3f7ae.js"
+  },
+  {
+    "revision": "9922fcae941e7132c21f",
+    "url": "/juko-landing-deploy/js/chunk-0471cfae.ee99cba3.js"
+  },
+  {
+    "revision": "03979835ea04b8b39629",
+    "url": "/juko-landing-deploy/js/chunk-2d6697bc.bc7dec31.js"
+  },
+  {
+    "revision": "1c9031c617ac1fb4ee7a",
+    "url": "/juko-landing-deploy/js/chunk-97d2034c.0b2a51b1.js"
+  },
+  {
+    "revision": "78d685e66440f4bebbe6",
+    "url": "/juko-landing-deploy/js/chunk-c5c8e94a.f6fce489.js"
+  },
+  {
+    "revision": "4142996c49335420c300",
+    "url": "/juko-landing-deploy/js/chunk-vendors.66676230.js"
+  },
+  {
+    "revision": "32b22254ef52aa202526ef855b58b294",
+    "url": "/juko-landing-deploy/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/juko-landing-deploy/robots.txt"
+  }
+]);
